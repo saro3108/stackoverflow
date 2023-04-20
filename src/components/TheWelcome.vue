28d3c07a6f091dbb1c61d0069231a21513534d0d
saro3108 <34979653+saro3108@users.noreply.github.com>
2023-04-20 21:20:06 +0530

    Ask Question

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,85 +1,88 @@
 <script setup>
-
-const data =  [
-    {
-       "title": "Clean the kitchen",
-       "description": "Mop the floor, wipe the countertop and don't forget to take out the trash!"
-    },
-    {
-       "title": "Call Mom",
-       "description": "It's her birthday!"
-    },
-    {
-       "title": "Water flowers",
-       "description": "They need water, or they will die."
-    },
-    {
-        "title": "Clean the kitchen",
-        "description": "Mop the floor, wipe the countertop and don't forget to take out the trash!"
-     },
-     {
-        "title": "Call Mom",
-        "description": "It's her birthday!"
-     },
-     {
-        "title": "Water flowers",
-        "description": "They need water, or they will die."
-     },
-     {
-        "title": "Clean the kitchen",
-        "description": "Mop the floor, wipe the countertop and don't forget to take out the trash!"
-     },
-     {
-        "title": "Call Mom",
-        "description": "It's her birthday!"
-     },
-     {
-        "title": "Water flowers",
-        "description": "They need water, or they will die."
-     }
- ] 
+const data = [
+  {
+    title: "Clean the kitchen",
+    description:
+      "Mop the floor, wipe the countertop and don't forget to take out the trash!"
+  },
+  {
+    title: "Call Mom",
+    description: "It's her birthday!"
+  },
+  {
+    title: "Water flowers",
+    description: "They need water, or they will die."
+  },
+  {
+    title: "Clean the kitchen",
+    description:
+      "Mop the floor, wipe the countertop and don't forget to take out the trash!"
+  },
+  {
+    title: "Call Mom",
+    description: "It's her birthday!"
+  },
+  {
+    title: "Water flowers",
+    description: "They need water, or they will die."
+  },
+  {
+    title: "Clean the kitchen",
+    description:
+      "Mop the floor, wipe the countertop and don't forget to take out the trash!"
+  },
+  {
+    title: "Call Mom",
+    description: "It's her birthday!"
+  },
+  {
+    title: "Water flowers",
+    description: "They need water, or they will die."
+  }
+];
 </script>
 
 <template>
-<div class="main-div">
-<button class="btn-style" @click="$router.push('/ask-question')">Ask Question</button>
-<div>
-<div class="card-view" v-for="item in data" :key="item.id">
-  <a href="/question-details"><div class="tittle-clr">{{item.title}}</div></a>
-  <div>{{item.description}}</div>
-</div>
-</div>
-</div>
- 
+  <div class="main-div">
+    <button class="btn-style" @click="$router.push('/ask-question')">Ask Question</button>
+    <div>
+      <div class="card-view" v-for="item in data" :key="item.id">
+        <a href="/question-details">
+          <div class="tittle-clr">{{item.title}}</div>
+        </a>
+        <div>{{item.description}}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <style>
 .main-div {
-    display: block;
-    margin: 35px auto;
-    padding: 20px;
+  display: block;
+  margin: 35px auto;
+  padding: 20px;
 }
 
 .card-view {
-   height: 75px;
-    font-size: 18px;
-    margin: auto;
-    border-top: 1px solid #00000038;
-    padding: 10px;
+  height: 75px;
+  font-size: 18px;
+  margin: auto;
+  border-top: 1px solid #00000038;
+  padding: 10px;
 }
 .card-view:first-child {
-   margin-top: 35px;
+  margin-top: 35px;
 }
 .btn-style {
-   width: 104px;
-    height: 34px;
-    background: #1010e4c2;
-    color: white;
-    border: none;
-    float: right;
+  width: 104px;
+  height: 34px;
+  background: #1010e4c2;
+  color: white;
+  border: none;
+  float: right;
 }
 .tittle-clr {
-   color: hsl(206,100%,52%);
+  color: hsl(206, 100%, 52%);
 }
 </style>
--- src/views/AskQuestion.vue
+++ src/views/AskQuestion.vue
@@ -1,6 +1,16 @@
+<script setup>
+ const isLetter = (e) => {
+      var inputValue = event.charCode;
+   if(!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)){
+       event.preventDefault();
+   }
+    }
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Ask a Public Question</h1>
+    <input type="text" class="input-style" v-model="firstName" v-on:keypress="isLetter($event)" maxlength="255" placeholder="Ask question">
   </div>
 </template>
 
@@ -8,8 +18,11 @@
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+  }
+  .input-style {
+    width: 450px;
+    height: 38px;
+    font-size: 18px;
   }
 }
 </style>
